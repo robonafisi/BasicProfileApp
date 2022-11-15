4f19b08362f6bb91adc6be2d0b0fd4237647bf58
Mohammad Nafisi <nafisidev@gmail.com>
2022-11-15 23:59:41 +0800

    Sending a new git

--- server/index.js
+++ server/index.js
@@ -11,55 +11,70 @@ app.use(express.json());
 
 //Create a user
 
-app.post("/users", async(req,res)=>{
+app.post("/create_user", async(req,res)=>{
   try {
-    const { full_name } = req.body;
+    const { first_name, last_name, email, company, street, city, state, zip, phone, birth_date, sex, date_entered } = req.body;
 
     const newTodo = await pool.query(
-      "INSERT INTO users (full_name) VALUES($1)",
-      [full_name]
+      "INSERT INTO users VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *",
+      [first_name, last_name, email, company, street, city, state, zip, phone, birth_date, sex, date_entered]
     );
 
-    res.json(newTodo);
-    console.log(newTodo);
+    res.json(newTodo.rows[0]);
 
   } catch (err) {
-    console.error(err.message);
+    console.error(error.message);
 }
 });
 
-app.get("/users2", async(req,res)=>{
+
+//List of all current users
+app.get("/list_user", async(req, res)=>{
   try {
-    const { full_name } = req.body;
 
-    const newTodo = await pool.query(
-      "INSERT INTO users (full_name) VALUES($1)",
-      [full_name]
-    );
+    const allusers = await pool.query("SELECT * FROM users");
+    res.json(allusers.rows);
 
-    res.json(newTodo);
-    console.log(newTodo);
+  } catch (error) {
+    console.error(error.message);
+  }
 
-  } catch (err) {
-    console.error(err.message);
+});
+
+//Get a user
+app.get("/users/:id", async(req, res)=>{
+  try {
+    const { id } = req.params;
+    const allusers = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    res.json(allusers.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+
+//Update customer's information
+app.put("/update_user/:id", async(req, res)=>{
+  try {
+    const { id } = req.params;
+    const { email } = req.body;
+    const updateUser = await pool.query("UPDATE users SET email = $1 WHERE id = $2", [email, id]);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+
+//Delete a customer
+app.delete("/delete_user/:id", async(req, res)=>{
+  try {
+  const { id } = req.params;
+  const deleteuser = await pool.query("DELETE FROM users WHERE id = $1",[id]);
+} catch (error) {
+  console.error(error.message);
 }
 });
 
-//List of all current customers
-
-
-//Find the full address based on name
-
-
-//Find the full-name based on user id
-
-
-//Find the company based on user id
-
-//Change the name of a contact
-
-
-//Change the email address
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
